a4cf27e80cd4057fa10a92d43cbe99329f2a7945
cyn9 <cihan.asci@tufts.edu>
2021-08-06 03:07:39 +0300

    Ring buffer implementation.

---
+++ 05-Templates/ring_buffer.cpp
@@ -0,0 +1,61 @@
+// Ring buffer implementation.
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Ring {
+    private:
+        int m_currentPos;
+        std::size_t m_size;
+        T *m_container;
+    
+    public:
+        Ring() : Ring(0) {}
+        Ring(std::size_t size) : m_currentPos {0}, 
+                                 m_size {size}, 
+                                 m_container {nullptr} {
+            // Make sure the container is empty.
+            assert(m_container == nullptr);
+            m_container = new T[m_size];
+        }
+
+        ~Ring() { delete[] m_container; }
+    
+        std::size_t getSize() const { return this->m_size; }
+
+        void add(T t) {
+            m_container[m_currentPos++] = t;
+            
+            if (m_currentPos == m_size) {
+                m_currentPos = 0;
+            }
+        }
+
+        T& get(std::size_t pos) const {
+            return m_container[pos];
+        }
+};
+
+template <typename T> void printRingBuffer(const Ring<T> &);
+
+int main() {
+    Ring<std::string> ring_test(4);
+
+    ring_test.add("one");
+    ring_test.add("two");
+    ring_test.add("three");
+    ring_test.add("four");
+    ring_test.add("five");
+
+    printRingBuffer(ring_test);
+
+    return 0;
+}
+
+template <typename T>
+void printRingBuffer(const Ring<T> &ring) {
+    for (auto i = 0; i < ring.getSize(); ++i) {
+        std::cout << i+1 << ": " << ring.get(i) << std::endl;
+    }
+}
